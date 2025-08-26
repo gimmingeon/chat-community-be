2562a33feab4a62b9e8f9aede51b488815811de8
gimmingeon <mnbqwe123987@naver.com>
2025-08-27 08:19:05 +0900

    Feat: user 엔티티 추가

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({
+    name: "user"
+})
+export class User {
+    @PrimaryGeneratedColumn({ unsigned: true })
+    id: number;
+
+    @Column({ type: "varchar", unique: true, nullable: false })
+    email: string;
+
+    @Column({ type: "varchar", nullable: false })
+    password: string;
+
+    @Column({ type: 'varchar', unique: true, nullable: false })
+    nickname: string;
+
+    @CreateDateColumn({ type: "timestamp" })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: "timestamp" })
+    updatedAt: Date;
+}
