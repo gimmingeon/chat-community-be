2a7fa2fee31d11ce86d5d1159b5b31f0dc403437
gimmingeon <mnbqwe123987@naver.com>
2025-08-27 08:07:12 +0900

    Feat: user 폴더 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get<string>("DB_HOST"),
+        port: +configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_NAME"),
+        synchronize: configService.get<boolean>("DB_SYNC"),
+        // 자동으로 엔티티를 불러오는 기능
+        autoLoadEntities: true,
+        logging: true
+      }),
+    }),
+    UserModule
+  ],
   controllers: [],
   providers: [],
 })
