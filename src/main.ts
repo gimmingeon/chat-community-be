abab51ca624b13dc05de6814adbcb3e2584e8395
gimmingeon <mnbqwe123987@naver.com>
2026-03-09 15:08:46 +0900

    Feat: enableCors 추가

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.enableCors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+
   await app.listen(3000).then(() => {
     console.log('Server is running on http://localhost:3000');
   }).catch((err) => {
